b52d0aad520ae1781e4d64695643c129de5f8184
rabia <rabiaangel9921@gmail.com>
2022-11-04 18:26:14 -0400

    Initial Commit

---
+++ Rabia_c0880823_LabTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rabia_c0880823_LabTest
+//
+//  Created by Rabia Bembi on 2022-11-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
